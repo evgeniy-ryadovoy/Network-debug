898ca239aedca3555aa8ca08b731de8876f30b75
evgeniy.ryadovoy <evgeniy.ryadovoy@gmail.com>
2021-08-22 18:10:52 +0600

    Initial Commit

---
+++ Network/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Network
+//
+//  Created by Evgeniy on 22.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
